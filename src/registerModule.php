9ac4cd7ba3d48b1eb8758e03dae5055681a17ce4
Faulo <FauloLio@gmail.com>
2018-03-29 20:14:24 +0200

    no longer a farah module

--- src/registerModule.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Slothsoft\Farah\Module\Module;
-use Slothsoft\Farah\Module\ModuleRepository;
-use Slothsoft\Farah\Module\FarahUrl\FarahUrlAuthority;
-
-ModuleRepository::getInstance()->registerModule(
-    new Module(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'w3c'),
-        dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets'));
