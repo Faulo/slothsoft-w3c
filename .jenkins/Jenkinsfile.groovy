52bdf4003f0588edc23babafd68a995e7486c87c
Daniel Schulz (Madeline) <faulolio@gmail.com>
2026-04-20 03:17:03 +0200

    build: update files

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -1,35 +1,22 @@
-def runTests(def versions) {
-	for (version in versions) {
-		stage("PHP: ${version}") {
-			dir('.reports') {
-				deleteDir()
-			}
+def unstable(def body) {
+	catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
+		body()
+	}
+}
 
-			docker.image("faulo/farah:${version}").inside {
-				catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
-					if (env.FARAH_INSTALL_FIREFOX == '1') {
-						if (isUnix()) {
-							// already part of the farah image
-						} else {
-							callShell "choco install Firefox --no-progress --yes --skip-checksums --params='/NoTaskbarShortcut /NoDesktopShortcut /NoStartMenuShortcut /NoAutoUpdate'"
-						}
-					}
-				}
+def runComposerTest(def version, def variant) {
+	unstable {
+		callShell "composer update --prefer-${variant}"
+		callShell "composer exec phpunit -- --log-junit .reports/${version}-${variant}.xml"
+	}
+}
 
-				catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
-					callShell 'composer update --prefer-lowest'
-					callShell "composer exec phpunit -- --log-junit .reports/${version}-lowest.xml"
-				}
-
-				catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
-					callShell 'composer update --prefer-stable'
-					callShell "composer exec phpunit -- --log-junit .reports/${version}-stable.xml"
-				}
-			}
-
-			dir('.reports') {
-				junit "*"
-			}
+def installFirefox() {
+	unstable {
+		if (isUnix()) {
+			// already part of the farah image
+		} else {
+			callShell "choco install Firefox --no-progress --yes --skip-checksums --params='/NoTaskbarShortcut /NoDesktopShortcut /NoStartMenuShortcut /NoAutoUpdate'"
 		}
 	}
 }
@@ -39,29 +26,64 @@ pipeline {
 	options {
 		disableConcurrentBuilds()
 		disableResume()
+		disableRestartFromStage()
+		skipDefaultCheckout()
 	}
 	environment {
 		COMPOSER_PROCESS_TIMEOUT = '3600'
 		FARAH_INSTALL_FIREFOX = '0'
 	}
 	stages {
-		stage('Linux') {
-			agent {
-				label 'docker && linux'
-			}
+		stage('Setup') {
 			steps {
 				script {
-					runTests(["7.4", "8.0", "8.1", "8.2", "8.3", "8.4", "8.5"])
-				}
-			}
-		}
-		stage('Windows') {
-			agent {
-				label 'docker && windows'
-			}
-			steps {
-				script {
-					runTests(["7.4", "8.0", "8.1", "8.2", "8.3", "8.4", "8.5"])
+					def platforms = ['linux', 'windows']
+					def versions = ["7.4", "8.0", "8.1", "8.2", "8.3", "8.4", "8.5"]
+					def variants = ['lowest', 'stable']
+
+					def branches = [:]
+
+					for (def p in platforms) {
+						def platform = p
+						for (def v in versions) {
+							def version = v
+							for (def a in variants) {
+								def variant = a
+
+								def name = "${platform} php-${version} ${variant}"
+								def label = "${platform} && docker"
+								def workspace = "php-${version}-${variant}"
+
+								branches[name] = {
+									stage(name) {
+										node(label) {
+											ws("${WORKSPACE}@${workspace}") {
+												checkout scm
+
+												dir('.reports') {
+													deleteDir()
+												}
+
+												docker.image("faulo/farah:${version}").inside {
+													if (env.FARAH_INSTALL_FIREFOX == '1') {
+														installFirefox()
+													}
+
+													runComposerTest(version, variant)
+												}
+
+												dir('.reports') {
+													junit "*.xml"
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+
+					parallel branches
 				}
 			}
 		}
